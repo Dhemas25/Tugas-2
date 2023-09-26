9b08d38fd68c730fd608763f06b764c1e515981e
Dhemas25 <dhemaswicaksono@gmail.com>
2023-09-26 21:07:17 +0700

    Push ke-1 untuk tugas 4

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,4 +7,6 @@ class Item(models.Model):
     name = models.CharField(max_length=255)   
     amount = models.IntegerField()
     description = models.TextField()
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
